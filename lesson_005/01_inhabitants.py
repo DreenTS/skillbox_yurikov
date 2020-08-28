04c18a89d94980f194ccb5c3e84d3e9a4511da29
Василий Юриков <loney2ears@yandex.ru>
2020-08-28 14:04:52 +1000

    Import rooms.

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -3,4 +3,11 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
+import room_1, room_2
+
+print('В комнате room_1 живут:')
+for name in enumerate(room_1.folks):
+    print(name[0] + 1, name[1])
+print('\nВ комнате room_2 живут:')
+for name in enumerate(room_2.folks):
+    print(name[0] + 1, name[1])
