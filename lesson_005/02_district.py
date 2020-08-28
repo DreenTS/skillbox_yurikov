1819d79b82dd92ac5882eeaaaa5a97fc41da5942
Василий Юриков <loney2ears@yandex.ru>
2020-08-28 14:43:24 +1000

    Import district.

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -5,4 +5,32 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+from district.central_street.house1 import room1 as central_h1_r1
+from district.central_street.house1 import room2 as central_h1_r2
+from district.central_street.house2 import room1 as central_h2_r1
+from district.central_street.house2 import room2 as central_h2_r2
+from district.soviet_street.house1 import room1 as soviet_h1_r1
+from district.soviet_street.house1 import room2 as soviet_h1_r2
+from district.soviet_street.house2 import room1 as soviet_h2_r1
+from district.soviet_street.house2 import room2 as soviet_h2_r2
+
+list_of_residents = []
+for name in central_h1_r1.folks:
+    list_of_residents.append(name)
+for name in central_h1_r2.folks:
+    list_of_residents.append(name)
+for name in central_h2_r1.folks:
+    list_of_residents.append(name)
+for name in central_h2_r2.folks:
+    list_of_residents.append(name)
+for name in soviet_h1_r1.folks:
+    list_of_residents.append(name)
+for name in soviet_h1_r2.folks:
+    list_of_residents.append(name)
+for name in soviet_h2_r1.folks:
+    list_of_residents.append(name)
+for name in soviet_h2_r2.folks:
+    list_of_residents.append(name)
+
+# Никак не додумался, как ещё можно сделать. Если есть способ - подскажите плз.
+print(', '.join(list_of_residents))
