bafdd3109158862473a13fee6e6d9c6f459d3c2e
Василий Юриков <loney2ears@yandex.ru>
2020-09-08 12:28:16 +1000

    Easy mod.

--- lesson_007/02_alchemy.py
+++ lesson_007/02_alchemy.py
@@ -19,7 +19,139 @@
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
+class Water:
+
+    def __init__(self):
+        self.name = 'Вода'
+
+    def __add__(self, other):
+        if other.name == 'Воздух':
+            return Storm()
+        elif other.name == 'Огонь':
+            return Steam()
+        elif other.name == 'Земля':
+            return Dirt()
+        else:
+            return None
+
+    def __str__(self):
+        return self.name
+
+
+class Air:
+    def __init__(self):
+        self.name = 'Воздух'
+
+    def __add__(self, other):
+        if other.name == 'Вода':
+            return Storm()
+        elif other.name == 'Огонь':
+            return Lightning()
+        elif other.name == 'Земля':
+            return Dust()
+        else:
+            return None
+
+    def __str__(self):
+        return self.name
+
+
+class Fire:
+    def __init__(self):
+        self.name = 'Огонь'
+
+    def __add__(self, other):
+        if other.name == 'Вода':
+            return Steam()
+        elif other.name == 'Воздух':
+            return Lightning()
+        elif other.name == 'Земля':
+            return Lava()
+        else:
+            return None
+
+    def __str__(self):
+        return self.name
+
+
+class Earth:
+    def __init__(self):
+        self.name = 'Земля'
+
+    def __add__(self, other):
+        if other.name == 'Вода':
+            return Dirt()
+        elif other.name == 'Воздух':
+            return Dust()
+        elif other.name == 'Огонь':
+            return Lava()
+        else:
+            return None
+
+    def __str__(self):
+        return self.name
+
+
+class Storm:
+
+    def __init__(self):
+        self.name = 'Шторм'
+
+    def __str__(self):
+        return self.name
+
+
+class Steam:
+
+    def __init__(self):
+        self.name = 'Пар'
+
+    def __str__(self):
+        return self.name
+
+
+class Dirt:
+
+    def __init__(self):
+        self.name = 'Грязь'
+
+    def __str__(self):
+        return self.name
+
+
+class Lightning:
+
+    def __init__(self):
+        self.name = 'Молния'
+
+    def __str__(self):
+        return self.name
+
+
+class Dust:
+
+    def __init__(self):
+        self.name = 'Пыль'
+
+    def __str__(self):
+        return self.name
+
+
+class Lava:
+
+    def __init__(self):
+        self.name = 'Лава'
+
+    def __str__(self):
+        return self.name
+
+
+print(Water(), '+', Air(), '=', Water() + Air())
+print(Water(), '+', Fire(), '=', Water() + Fire())
+print(Water(), '+', Earth(), '=', Water() + Earth())
+print(Air(), '+', Fire(), '=', Air() + Fire())
+print(Air(), '+', Earth(), '=', Air() + Earth())
+print(Fire(), '+', Earth(), '=', Fire() + Earth())
 
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
