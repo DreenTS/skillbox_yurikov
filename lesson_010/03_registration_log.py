b00f0a47c6ae663874ca625a689972334bcaa0d3
Василий Юриков <loney2ears@yandex.ru>
2020-09-23 15:52:52 +1000

    Registration log.

--- lesson_010/03_registration_log.py
+++ lesson_010/03_registration_log.py
@@ -22,4 +22,42 @@
 # - поле возраст НЕ является числом от 10 до 99: ValueError
 # Вызов метода обернуть в try-except.
 
-# TODO здесь ваш код
+class NotNameError(Exception):
+
+    def __str__(self):
+        return 'NotNameError: the name field contains more than just letters'
+
+
+class NotEmailError(Exception):
+
+    def __str__(self):
+        return 'NotEmailError: the email field does not contain @ or . (dot)'
+
+
+def validation_method(one_line):
+    if len(one_line) != 3:
+        raise ValueError
+    elif not one_line[0].isalpha():
+        raise NotNameError
+    elif '@' not in one_line[1] or '.' not in one_line[1]:
+        raise NotEmailError
+    elif int(one_line[2]) not in range(10, 100):
+        raise ValueError
+
+
+with open('registrations.txt', 'r', encoding='utf8') as file:
+    for line in file:
+        try:
+            validation_method(line.split())
+            with open('registrations_good.log', 'a', encoding='utf8') as good:
+                good.write(line[:-1] + '\n')
+        except ValueError as exc:
+            with open('registrations_bad.log', 'a', encoding='utf8') as bad:
+                bad.write(f'[Пользователь {line[:-1]}. Исключение ValueError: all three fields are not present, '
+                          f'\nor the age field is not a number from 10 to 99.]\n')
+        except NotNameError as exc:
+            with open('registrations_bad.log', 'a', encoding='utf8') as bad:
+                bad.write(f'[Пользователь {line[:-1]}. Исключение {exc}]\n')
+        except NotEmailError as exc:
+            with open('registrations_bad.log', 'a', encoding='utf8') as bad:
+                bad.write(f'[Пользователь {line[:-1]}. Исключение {exc}]\n')
