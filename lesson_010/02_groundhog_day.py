813ceb7d0a62990aaad58272e5d2412659968a11
Василий Юриков <loney2ears@yandex.ru>
2020-09-23 15:00:36 +1000

    Carma and exceptions.

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -16,9 +16,67 @@
 # кармы до уровня ENLIGHTENMENT_CARMA_LEVEL. Исключения обработать и записать в лог.
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
+import random
+
+
+class IamGodError(Exception):
+
+    def __str__(self):
+        return 'Он возомнил себя Богом. Все его отвергли. Снова.'
+
+
+class DrunkError(Exception):
+
+    def __str__(self):
+        return 'Он напился и его тело того не выдержало. Всё по-новой.'
+
+
+class CarCrashError(Exception):
+
+    def __str__(self):
+        return 'Он разбился в автокатастрофе. В следующий раз повезёт.'
+
+
+class GluttonyError(Exception):
+
+    def __str__(self):
+        return 'Он умер от переедания. Попробуем ещё раз.'
+
+
+class DepressionError(Exception):
+
+    def __str__(self):
+        return 'Депрессия опасная штука. Ещё один шанс.'
+
+
+class SuicideError(Exception):
+
+    def __str__(self):
+        return 'Он покончил с собой. Пусть попробует ещё разок.'
+
+
+def one_day(choice_list):
+    if random.randint(1, 13) == 13:
+        exc = random.choice(choice_list)
+        raise exc
+    else:
+        return random.randint(1, 7)
+
 
 ENLIGHTENMENT_CARMA_LEVEL = 777
 
-# TODO здесь ваш код
+exc_list = [IamGodError, DrunkError, CarCrashError, GluttonyError, DepressionError, SuicideError]
+curr_carma = 0
+days = 0
+while True:
+    days += 1
+    if curr_carma >= ENLIGHTENMENT_CARMA_LEVEL:
+        break
+    try:
+        curr_carma += one_day(exc_list)
+    except Exception as exc:
+        with open('reloads.txt', 'a', encoding='utf8') as file:
+            file.write(f'[День {days}. {exc}]\n')
+print(f'Ура! carma = {curr_carma}, вышли из цикла на {days} день.')
 
 # https://goo.gl/JnsDqu
