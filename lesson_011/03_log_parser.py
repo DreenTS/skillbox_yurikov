b1c411f24362cf774ea9763a9b75c48fb4ed28ff
Василий Юриков <loney2ears@yandex.ru>
2020-09-24 21:59:35 +1000

    Log parser: generator + iterator.

--- lesson_011/03_log_parser.py
+++ lesson_011/03_log_parser.py
@@ -14,4 +14,57 @@
 #
 # [2018-05-17 01:57] 1234
 
-# TODO здесь ваш код
+
+# Решение генератором
+
+def log_parser(file_name):
+    log_dict = {}
+    temp_line = ''
+    with open(file_name, 'r', encoding='cp1251') as file:
+        for line in file:
+            if temp_line != line[1:17] and temp_line:
+                yield temp_line, log_dict[temp_line]
+            temp_line = line[1:17]
+            if temp_line not in log_dict:
+                log_dict[temp_line] = 0
+            if line[-4] == 'N':
+                log_dict[temp_line] += 1
+
+
+grouped_events = log_parser(file_name='events.txt')
+for group_time, event_count in grouped_events:
+    print(f'[{group_time}] {event_count}')
+
+
+# Решение итератором
+
+# class LogParser:
+#
+#     def __init__(self, file_name):
+#         self.file_in = file_name
+#         self.log_dict = {}
+#         self.file = None
+#         self.line = ''
+#
+#     def __iter__(self):
+#         self.file = open(self.file_in, 'r', encoding='cp1251')
+#         self.line = self.file.readline()
+#         return self
+#
+#     def __next__(self):
+#         temp_line = self.line[1:17]
+#         while temp_line == self.line[1:17]:
+#             if not self.line:
+#                 self.file.close()
+#                 raise StopIteration
+#             if self.line[1:17] not in self.log_dict:
+#                 self.log_dict[self.line[1:17]] = 0
+#             if self.line[-4] == 'N':
+#                 self.log_dict[temp_line] += 1
+#             self.line = self.file.readline()
+#         return temp_line, self.log_dict[temp_line]
+#
+#
+# grouped_events = LogParser(file_name='events.txt')
+# for group_time, event_count in grouped_events:
+#     print(f'[{group_time}] {event_count}')
