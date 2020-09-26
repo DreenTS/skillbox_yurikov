eee303cccd713cac66496c2d194b9bbcb7604f32
Василий Юриков <loney2ears@yandex.ru>
2020-09-26 12:26:49 +1000

    Log decorator + hard part.

--- lesson_011/04_error_log_decorator.py
+++ lesson_011/04_error_log_decorator.py
@@ -7,21 +7,45 @@
 # Формат лога: <имя функции> <параметры вызова> <тип ошибки> <текст ошибки>
 # Лог файл открывать каждый раз при ошибке в режиме 'a'
 
+# Усложненное задание (делать по желанию).
+# Написать декоратор с параметром - именем файла
+#
+# @log_errors('function_errors.log')
+# def func():
+#     pass
 
-def log_errors(func):
-    pass
-    # TODO здесь ваш код
+from pprint import pprint
+
+
+def log_errors_with_file(file_name):
+    def log_errors(func):
+
+        def surrogate(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+            except ValueError as exc:
+                with open(file_name, 'a', encoding='utf8') as file:
+                    file.write(f'[{func}: {args, kwargs}. ValueError: {exc}]\n')
+                raise ValueError(f'{func}: {args, kwargs}. ValueError: {exc}')
+            except ZeroDivisionError as exc:
+                with open(file_name, 'a', encoding='utf8') as file:
+                    file.write(f'[{func}: {args, kwargs}. ZeroDivisionError: {exc}]\n')
+                raise ZeroDivisionError(f'{func}: {args, kwargs}. ZeroDivisionError: {exc}')
+            return result
+
+        return surrogate
+    return log_errors
 
 
 # Проверить работу на следующих функциях
-@log_errors
-def perky(param):
-    return param / 0
+@log_errors_with_file(file_name='function_errors.log')
+def perky(*args):
+    return args[0] / 0
 
 
-@log_errors
-def check_line(line):
-    name, email, age = line.split(' ')
+@log_errors_with_file(file_name='function_errors.log')
+def check_line(*args):
+    name, email, age = str(args[0]).split(' ')
     if not name.isalpha():
         raise ValueError("it's not a name")
     if '@' not in email or '.' not in email:
@@ -38,17 +62,19 @@ lines = [
     'Земфира 86',
     'Равшан wmsuuzsxi@mail.ru 35',
 ]
+ok_list = []
+
 for line in lines:
     try:
         check_line(line)
+        ok_list.append(line)
     except Exception as exc:
-        print(f'Invalid format: {exc}')
-perky(param=42)
+        print(f'[Error: {exc}]')
+print('OK for:')
+pprint(ok_list)
 
+try:
+    perky(42)
+except Exception as exc:
+    print(f'[Error: {exc}]')
 
-# Усложненное задание (делать по желанию).
-# Написать декоратор с параметром - именем файла
-#
-# @log_errors('function_errors.log')
-# def func():
-#     pass
