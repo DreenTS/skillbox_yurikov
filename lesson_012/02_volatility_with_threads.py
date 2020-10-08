b7aeab4bb051f9111a4b8341a50580b457eef768
Василий Юриков <loney2ears@yandex.ru>
2020-10-08 18:39:41 +1000

    Volatility with threads.

--- lesson_012/02_volatility_with_threads.py
+++ lesson_012/02_volatility_with_threads.py
@@ -17,6 +17,75 @@
 #       ТИКЕР7, ТИКЕР8, ТИКЕР9, ТИКЕР10, ТИКЕР11, ТИКЕР12
 # Волатильности указывать в порядке убывания. Тикеры с нулевой волатильностью упорядочить по имени.
 #
-# TODO Внимание! это задание можно выполнять только после зачета lesson_012/01_volatility.py !!!
 
-# TODO тут ваш код в многопоточном стиле
+import os
+import time
+from threading import Thread
+
+
+class CheckVolatility(Thread):
+
+    def __init__(self, dir_path, file_name, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.dir_path = dir_path
+        self.file_name = file_name
+        self.maximum = 0
+        self.minimum = 0
+        self.half_sum = 0
+        self.volatility = 0
+
+    def run(self):
+        with open(os.path.join(self.dir_path, self.file_name), 'r', encoding='utf8') as file:
+            file.readline()
+            temp_list = []
+            for line in file:
+                temp_list.append(float(line.split(',')[2]))
+            self.maximum = max(temp_list)
+            self.minimum = min(temp_list)
+            self.half_sum = (self.maximum + self.minimum) / 2
+            self.volatility = ((self.maximum - self.minimum) / self.half_sum) * 100
+
+
+class Manager(Thread):
+
+    def __init__(self, files, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.files = files
+        self.class_list = []
+        self.zero_list = []
+        self.threads = []
+
+    def run(self):
+        for dirpath, dirnames, filenames in os.walk(self.files):
+            for filename in filenames:
+                self.threads.append(CheckVolatility(dir_path=dirpath, file_name=filename))
+            for thread in self.threads:
+                thread.start()
+            for thread in self.threads:
+                thread.join()
+            for thread in self.threads:
+                if thread.volatility != 0.0:
+                    self.class_list.append([thread.file_name[:-4], thread.volatility])
+                else:
+                    self.zero_list.append(thread.file_name[:-4])
+        self.show_volatility()
+
+    def show_volatility(self):
+        self.class_list.sort(key=lambda i: i[1])
+        self.class_list.reverse()
+        print('Максимальная волатильность:')
+        for ticker in self.class_list[:3]:
+            print(f'{ticker[0]} - {round(ticker[1], 3)} %')
+        print('\nМинимальная волатильность:')
+        for ticker in self.class_list[-3:]:
+            print(f'{ticker[0]} - {round(ticker[1], 3)} %')
+        print('\nНулевая волатильность:')
+        self.zero_list.sort()
+        print(','.join(self.zero_list))
+
+
+tt = time.time()
+manager = Manager('trades')
+manager.start()
+manager.join()
+print(time.time() - tt)
