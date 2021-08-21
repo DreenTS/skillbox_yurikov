1c53877ed732a3f9ef5712492eed5086fd66be76
Юриков Василий <loney2ears@yandex.ru>
2021-08-21 15:43:41 +1000

    Added the get_score function

---
+++ lesson_014/bowling.py
@@ -0,0 +1,11 @@
+def get_score(game_result="--------------------"):
+    if not isinstance(game_result, str):
+        raise TypeError('параметр game_result должен иметь тип "str"')
+
+    # Замена русской буквы на латинскую (проверк на дурака)
+    # Для удобства подсчёта очков: заменяем фрейм страйка 'X' на фрейм '-X'
+    result = game_result.upper()
+    result.replace('Х', 'X').replace('X', '-X')
+
+
+
