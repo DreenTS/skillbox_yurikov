fed86460325fec3ba60d27c66cc6aea1e78359af
Юриков Василий <loney2ears@yandex.ru>
2021-08-31 19:55:32 +1000

    Added implementation for public counting + minor fixes

--- lesson_014/bowling.py
+++ lesson_014/bowling.py
@@ -30,7 +30,7 @@ class FrameSumError(Exception):
         self.f = f
 
     def __str__(self):
-        return f'Ошибка в записи фрейма "{self.f}"; сумма должна быть <= 9'
+        return f'Ошибка в записи фрейма "{self.f}"; сумма должна быть <= 10'
 
 
 class SpareError(Exception):
@@ -49,7 +49,7 @@ class WrongModeError(Exception):
 
 
 def data_check(data):
-    default_set = set('-123456789/X')
+    default_set = set('0123456789/X')
     if len(data) != 20:
         raise FrameLengthError
     elif not set(data) <= default_set:
@@ -60,7 +60,7 @@ def frames_check(frame_list):
     for f in frame_list:
         if f.isdigit() and sum(map(int, f)) > 10:
             raise FrameSumError(f=f)
-        elif '/' in f and f[0] in ['-', '/']:
+        elif '/' in f and f[0] in ['0', '/']:
             raise SpareError(f=f)
 
 
@@ -73,8 +73,8 @@ def get_score(game_result=None, mode='private'):
         raise WrongModeError
 
     # Замена русской буквы на латинскую (проверк на дурака)
-    # Для удобства подсчёта очков: заменяем фрейм страйка 'X' на фрейм '-X'
-    result = game_result.upper().replace('Х', 'X').replace('X', '-X')
+    # Для удобства подсчёта очков: заменяем фрейм страйка 'X' на фрейм '0X', промах по кеглям '-' заменяем на '0'
+    result = game_result.upper().replace('Х', 'X').replace('X', '0X').replace('-', '0')
 
     # Проверка данных на валидность
     data_check(data=result)
@@ -94,7 +94,7 @@ if __name__ == '__main__':
         game_result = '8/549-XX5/53629/9/'
         mode = 'public'
         scores = get_score(game_result=game_result, mode=mode)
-        print(f'Количество очков для результата "{game_result}" - {scores}.'
+        print(f'Количество очков для результата "{game_result}" - {scores}.\n'
               f'Подсчёт очков был произведён согласно правилам {RULES[mode]} рынка.')
     except Exception as exc:
         print(f'{exc.__class__.__name__}: {exc}')
--- lesson_014/state_handler.py
+++ lesson_014/state_handler.py
@@ -3,6 +3,11 @@ from abc import ABC, abstractmethod
 
 class State(ABC):
 
+    def __init__(self, is_spare=False, is_strike=False):
+        self.res = 0
+        self.is_spare = is_spare
+        self.is_strike = is_strike
+
     @abstractmethod
     def private_count(self, char):
         pass
@@ -14,34 +19,42 @@ class State(ABC):
 
 class FirstThrow(State):
 
-    def __init__(self):
-        self.res = 0
-
     def private_count(self, char):
-        if char != '-':
-            self.res = int(char)
+        self.res = int(char)
         return SecondThrow(), self.res
 
     def public_count(self, char):
-        pass
+        if self.is_spare:
+            self.is_spare = False
+            self.res = int(char)
+            return self, self.res
+        if self.is_strike:
+            self.is_strike = False
+        self.res = int(char)
+        return SecondThrow(is_spare=self.is_spare, is_strike=self.is_strike), self.res
 
 
 class SecondThrow(State):
-
-    def __init__(self):
-        self.res = 0
 
     def private_count(self, char):
         if char == 'X':
             self.res = 20
         elif char == '/':
             self.res = 15
-        elif char != '-':
+        else:
             self.res = int(char)
         return FirstThrow(), self.res
 
     def public_count(self, char):
-        pass
+        if char == '/':
+            self.res = 10
+            self.is_spare = True
+        elif char == 'X':
+            self.res = 10
+            self.is_strike = True
+        else:
+            self.res = int(char)
+        return FirstThrow(is_spare=self.is_spare, is_strike=self.is_strike), self.res
 
 
 class ScoreHandler:
@@ -53,7 +66,7 @@ class ScoreHandler:
         self.total_score = 0
 
     def count_score(self):
-        self.state, prev, curr = FirstThrow(), 0, 0
+        self.state, prev, curr, local_state = FirstThrow(), 0, 0, None
         if self.mode == 'private':
             for char in self.result:
                 self.state, curr = self.state.private_count(char=char)
@@ -63,4 +76,29 @@ class ScoreHandler:
                     self.total_score += curr
                 prev = curr
         else:
-            pass
+            for i in range(len(self.result)):
+                if i == 18 and ('X' in self.result[i:i+2] or '/' in self.result[i:i + 2]):
+                    self.total_score += 10
+                    break
+                else:
+                    self.state, curr = self.state.public_count(char=self.result[i])
+                    if self.state.is_spare:
+                        self.state, curr = self.state.public_count(char=self.result[i + 1])
+                        self.total_score += curr + 10 - prev
+                        self.state.res = 0
+                    elif self.state.is_strike:
+                        local_state = self.state
+                        self.total_score += curr
+                        self.state, prev = self.state.public_count(char=self.result[i + 1])
+                        self.state, curr = self.state.public_count(char=self.result[i + 2])
+                        if self.state.is_spare or self.state.is_strike:
+                            self.total_score += 10
+                            if self.state.is_strike:
+                                self.state, curr = self.state.public_count(char=self.result[i + 3])
+                                self.total_score += curr
+                        else:
+                            self.total_score += prev + curr
+                        self.state = local_state
+                    else:
+                        self.total_score += curr
+                        prev = curr
