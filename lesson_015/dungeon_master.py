0a18975d21bbac2db8766df1d43053fc73fc26b2
Юриков Василий <loney2ears@yandex.ru>
2021-09-10 15:44:37 +1000

    Added an inner loop to start the game process;  updated game process functions

--- lesson_015/dungeon_master.py
+++ lesson_015/dungeon_master.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import time
 from decimal import Decimal, getcontext
@@ -18,23 +19,17 @@ class DungeonMaster:
         self.map = dung_map[self.player.loc]
         self.str_remaining_time = time
         self._str_to_decimal_time()
-        self.flags = {
-            self._game: True,
-            self._fight: False,
-        }
         self._time_pattern = r'tm\d*\.{,1}\d*'
         self._exp_pattern = r'exp\d*\.{,1}\d*'
 
     def tell(self):
         self._start()
-        if self.decimal_remaining_time == Decimal(0.0):
-            self._lose()
-        else:
-            for func, flag in self.flags.items():
-                if flag:
-                    self.flags[func] = False
-                    func()
-                    return
+        while self.player.loc != 'Hatch_tm159.098765432':
+            if self.decimal_remaining_time <= Decimal(0.0):
+                self._lose()
+                return
+            else:
+                self._game()
 
     def _str_to_decimal_time(self):
         getcontext().prec = 10
@@ -45,13 +40,21 @@ class DungeonMaster:
         for i, point in enumerate(self.map):
             if isinstance(point, dict):
                 print(f'{i + 1}. Перейти в локацию {list(point.keys())[0]}. '
-                      f'На это уйдёт {self._get_time_from_pattern(re_str=list(point.keys())[0])} секунд.')
+                      f'На это уйдёт {self._get_time_from_pattern(re_str=point)} секунд.')
             else:
                 print(f'{i + 1}. Атаковать {point} за {self._get_exp_from_pattern(re_str=point)} опыта. '
                       f'Требуется времени для битвы: {self._get_time_from_pattern(re_str=point)}.')
 
-        choice = int(input('Выберите действие (номер): '))
+        choice = int(input('Выберите действие (номер): ')) - 1
         if isinstance(choice, int) and choice in range(len(self.map) + 1):
+            action = self.map[choice]
+            temp_time = self._get_time_from_pattern(re_str=action)
+            self.player.date = str(datetime.timedelta(float(temp_time)))
+            self.decimal_remaining_time -= Decimal(temp_time)
+            if isinstance(action, dict):
+                self._travel(action=action)
+            else:
+                self._fight(action=action)
             os.system('cls')
         else:
             print('\nПохоже, вы написали неправильный номер. Давайте попробуем ещё раз...')
@@ -59,19 +62,25 @@ class DungeonMaster:
                 print('.', end='')
                 time.sleep(1.5)
             os.system('cls')
-            self._game()
 
     def _get_time_from_pattern(self, re_str):
-        return re.search(self._time_pattern, re_str).group()[2:]
+        if isinstance(re_str, dict):
+            return re.search(self._time_pattern, list(re_str.keys())[0]).group()[2:]
+        else:
+            return re.search(self._time_pattern, re_str).group()[2:]
 
     def _get_exp_from_pattern(self, re_str):
         return re.search(self._exp_pattern, re_str).group()[3:]
 
-    def _fight(self):
-        pass
+    def _travel(self, action):
+        self.player.loc = list(action.keys())[0]
+        self.map = action[self.player.loc]
+
+    def _fight(self, action):
+        self.player.exp += int(self._get_exp_from_pattern(re_str=action))
+        self.map.remove(action)
 
     def _start(self):
-        self.flags[self._game] = True
         print('start')
 
     def _lose(self):
@@ -90,4 +99,3 @@ if __name__ == '__main__':
     dungeon_map = {'Location_0_tm0': []}
     master = DungeonMaster(player='Nick', dung_map=dungeon_map, time='123456.0987654321')
     master.tell()
-    print(master.flags)
