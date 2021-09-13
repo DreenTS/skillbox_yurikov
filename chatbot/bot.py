b8ab1da89d7c24d1fb2314950ed1e36c2c33246e
Юриков Василий <loney2ears@yandex.ru>
2021-09-13 16:58:45 +1000

    Added docstrings for bot.py script

--- chatbot/bot.py
+++ chatbot/bot.py
@@ -16,7 +16,11 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 class UserState:
     """
     Состояние пользователя внутри сценария.
+    Атрибуты класса: име текущего сценария, имя текуего шага,
+    контекст (словарь с ответами пользователя).
+    Атрибуты класса принимают новые значения при каждом продвижении пользователя по сценарию.
     """
+
     def __init__(self, scenario_name, step_name, context=None):
         self.scenario_name = scenario_name
         self.step_name = step_name
@@ -25,7 +29,8 @@ class UserState:
 
 class Bot:
     """
-    Чат-бот для vk.com
+    Бот для vk.com
+    Позволяет оформить заказ авиабилета.
 
     Use python3.9
     """
@@ -35,6 +40,7 @@ class Bot:
         :param group_id: group id для группы vk.com
         :param token: token для группы vk.com
         """
+
         self.group_id = group_id
         self.token = token
         self.bot_logger = logger
@@ -47,6 +53,7 @@ class Bot:
         """
         Запуск бота.
         """
+
         for event in self.long_poller.listen():
             print('\nНОВОЕ СОБЫТИЕ:\n')
             try:
@@ -57,9 +64,28 @@ class Bot:
     def on_event(self, event):
         """
         Обработка событий из личных сообщений с сообществом.
+
+        Если пришедшее сообщение содержится в интентах, вызывается соответствующий сценарий.
+        Если при этом пользователь, от которого пришло сообщение, уже содержится в self.user_states,
+        пользователь удаляется из словаря.
+        Если сообщение не соответствует ни одному из интентов, пользователю отправляется случайное
+        сообщение из ответов по умолчанию.
+
+        Старт сценария  - вызов метода self.start_scenario()
+
+        Если сообщение пользователя не содержится в интентах, но сам пользователь есть в словаре self.user_states,
+        вызывается метод продолженя сценария - self.continue_scenario()
+
+        Оба основных метода (старта и продолжения сценария) возвращают строку - ответ пользователю.
+        Подробнее о формировании ответа см. функции обработчиков в handlers.py и второстепенных методах класса Bot.
+
+        Получив сформированный ответ пользователя от self.start_scenario(), self.continue_scenario()
+        или ответов по умолчанию, бот отправляет пользователю ответ.
+
         :param event: VkBotMessageEvent object
         :return: None
         """
+
         if event.type != VkBotEventType.MESSAGE_NEW:
             self.bot_logger.ERROR(f'Мы пока не умеем обрабатывать события типа {event.type}')
         user_id = event.object.message['peer_id']
@@ -86,12 +112,42 @@ class Bot:
                                peer_id=user_id)
 
     def start_scenario(self, user_id, scenario):
+        """
+        Метод старта сценария.
+
+        В словарь пользователей добавляется текущий пользователь, ему присваивается имя сценария,
+        имя текущего шага (берётся из самого сценария).
+        Возвращает значение поля text из текущего сценария на текущем шаге.
+
+        :param user_id: int, id пользователя
+        :param scenario: str, имя текущего сценария
+        :return: str, ответ пользователю из текста сценария
+        """
+
         step = settings.SCENARIOS[scenario]['first_step']
         self.user_states[user_id] = UserState(scenario_name=scenario, step_name=step)
         result = settings.SCENARIOS[scenario]['steps'][step]['text']
         return result
 
     def continue_scenario(self, user_id, user_text):
+        """
+        Метод продолжения сценария.
+
+        Вызывается обработчк из поля handler текущего шага сценария.
+        Если имя обработчика city_handler - к аргументам для вызова обработчика добавляется дополнительный
+        аргумент step - подробнее об этом см. docstrings обработчика.
+        Если обработчик возвращает False, возвращается текст из поля failure_text из текущего шага.
+        Если обработчик возвращает True, вызывается метод self.extra_print()
+        (подробнее про метод см. docstrings метода),
+        к возвращённому методом значению конкатенирует значение из поля text следующего шага сценария.
+        Если следующий шаг is None, пользователь удалется из словаря пользователей,
+        возвращается значение поля text текущего шага сценария. Таким образом, сценарий завершается.
+
+        :param user_id: int, id пользователя
+        :param user_text: str, сообщение пользователя
+        :return: str, ответ пользователю из текста сценария после обработки хендлером
+        """
+
         state = self.user_states[user_id]
 
         steps = settings.SCENARIOS[state.scenario_name]['steps']
@@ -122,6 +178,27 @@ class Bot:
         return result
 
     def extra_print(self, state, step):
+        """
+        Метод вывода допольнительного текста для ответа пользователя.
+
+        Нужен для отдельных шагов -
+        4_flight_selection (выбор из списка рейсов),
+        7_data_checking (подтверждение введённой информации).
+
+        Если текущий шаг == выбор из списка рейсов, вызывается метод self.dispatcher()
+        (подробнее о работе метода см. docstrings метода).
+        Возвращает строку - либо сообщение о том, что подходящих рейсов нет
+        (сценарий полностью завершается в методе self.continue_scenario() выше по стеку),
+        либо список подходящих рейсов.
+
+        Если текущий шаг == подтверждение введённой информации, возвращает строку - конкатенацию всех данных
+        пользователя (его выборов из предыдущих шагов сценария) из конекста пользователя.
+
+        :param state: UserState, объект пользователя из словаря пользователей
+        :param step: str, имя текущего шага
+        :return: str, дополнительный текст для ответ пользователю
+        """
+
         context = state.context
         res = ''
         if '4' in step:
@@ -143,6 +220,23 @@ class Bot:
         return res
 
     def dispatcher(self, context, from_city, to_city):
+        """
+        Метод диспетчер.
+
+        Проходится по всем подступным рейсам от города from_city до города to_city.
+        Если дата рейса в представлении datetime.datetime.date() отличается от даты заказа авиабилета
+        и желаемой даты рейса от пользователя не более, чем на 10 дней, конкатенирует дату рейса и стоимость билета
+        с возвращаемым значением.
+        Если таких рейсов не найдётся, возвращает пустую строку.
+
+        Возвращаемая строка обрабатывается в методе выше по стеку - self.extra_print()
+
+        :param context: dict, словарь контекста пользователя
+        :param from_city: str, ранее выбранный пользователем город отправления
+        :param to_city: str, ранее выбранный пользователем город прибытия
+        :return: str, список подходящих рейсов
+        """
+
         flights_list = [list(key.values()) for key in settings.FLIGHTS[from_city][to_city]]
         today = datetime.datetime.today().date()
         user_date_datetime_format = datetime.datetime.strptime(context['date'], '%d-%m-%Y').date()
