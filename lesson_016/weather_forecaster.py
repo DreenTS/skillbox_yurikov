3c474c9f532078de5d49873a4d5ee0da7513118e
Юриков Василий <loney2ears@yandex.ru>
2021-09-18 22:56:05 +1000

    Added implementation of input and processing of date for forecasting + minor fixes

--- lesson_016/weather_forecaster.py
+++ lesson_016/weather_forecaster.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import re
 import time
@@ -14,15 +15,33 @@ class WrongCityNameError(Exception):
         return '\nWrongCityNameError: имя введённого населённого пункта не найдено в базе.'
 
 
+class WrongDateRangeError(Exception):
+
+    def __str__(self):
+        return '\nWrongDateRangeError: введён неправильный диапазон дат: ' \
+               'нижняя граница должна быть строго меньше верхней границы.'
+
+
 class WeatherForecaster:
 
-    def __init__(self, date_for_forecast):
+    def __init__(self, date_for_forecast=None):
         self.data_for_parsing = settings.DATA_FOR_PARSING
-        self.date_for_forecast = date_for_forecast
+        self.date_for_forecast = date_for_forecast or []
         self.city_for_forecast = None
 
     def forecast(self):
         self._user_location_choice()
+        if not self.date_for_forecast:
+            self._date_input()
+        else:
+            self.date_for_forecast = self.date_for_forecast.split('-')
+            try:
+                self._date_check(dates=self.date_for_forecast)
+            except (ValueError, WrongDateRangeError) as exc:
+                self.date_for_forecast.clear()
+                print(f'{exc}\nДата должна быть в формате ДД.ММ или ДД.ММ-ДД.ММ , Давайте попробуем ещё раз', end='')
+                self._dots()
+                self._date_input()
 
     def _user_location_choice(self):
         is_ok_choice = False
@@ -30,6 +49,8 @@ class WeatherForecaster:
             user_input = input('\nВведите название населённого пункта: ')
             formatted_user_input = user_input.lower().replace('ё', 'е')
             try:
+                if formatted_user_input[0].isdigit():
+                    raise WrongCityNameError
                 self.city_for_forecast = self._get_city_from_network(formatted_user_input)
                 is_ok_choice = True
             except WrongCityNameError as exc:
@@ -39,6 +60,7 @@ class WeatherForecaster:
     def _get_city_from_network(self, user_choice):
         city = {}
         eng_letter = settings.LETTERS_IN_TRANSCRIPTION[user_choice[0].upper()]
+
         response = requests.get(self.data_for_parsing['cities_list_url'] + eng_letter)
         if response.status_code == 200:
             html_doc = BeautifulSoup(response.text, features='html.parser')
@@ -53,6 +75,35 @@ class WeatherForecaster:
             else:
                 raise WrongCityNameError
         return city
+
+    def _date_input(self):
+        while not self.date_for_forecast:
+            print('\nДата прогноза.')
+            user_dates = input('Введите дату в формате ДД.ММ,\n'
+                               'или диапазон дат в формате ДД.ММ-ДД.ММ (текущий год): ').split('-')
+            try:
+                self._date_check(dates=user_dates)
+            except (ValueError, WrongDateRangeError) as exc:
+                self.date_for_forecast.clear()
+                print(f'{exc}\nДата должна быть в формате ДД.ММ или ДД.ММ-ДД.ММ , Давайте попробуем ещё раз', end='')
+                self._dots()
+
+    def _date_check(self, dates):
+        for date in dates:
+            date += '.2021'
+            self.date_for_forecast.append(datetime.datetime.strptime(date, '%d.%m.%Y').date())
+
+        if len(self.date_for_forecast) > 2:
+            raise ValueError
+        elif len(self.date_for_forecast) == 2:
+            if self.date_for_forecast[1] - self.date_for_forecast[0] <= datetime.timedelta(days=0):
+                raise WrongDateRangeError
+
+        today = datetime.date.today()
+        for date in self.date_for_forecast:
+            if date - today > datetime.timedelta(days=13):
+                raise ValueError(f'Максимальная дата прогноза - сегодняшняя дата '
+                                 f'({datetime.date.strftime(today, "%d.%m.%Y")}) + 2 недели.\nНапоминание:')
 
     def _dots(self):
         """
@@ -71,3 +122,4 @@ if __name__ == '__main__':
     forecaster = WeatherForecaster(date_for_forecast='')
     forecaster.forecast()
     print(forecaster.city_for_forecast)
+    print(forecaster.date_for_forecast)
