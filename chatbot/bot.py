57448dd80fa2be53725765988ffabf8820dc72d7
Юриков Василий <loney2ears@yandex.ru>
2021-09-28 17:18:40 +1000

    Deleted UserState class; added _create_and_send_ticket method; minor fixes + updated docstrinds

--- chatbot/bot.py
+++ chatbot/bot.py
@@ -1,6 +1,11 @@
 import datetime
 
+import requests
+from pony.orm import db_session
+
 import handlers
+from models import UserState, Registration
+from ticket_maker import TicketMaker
 
 try:
     import settings
@@ -11,20 +16,6 @@ import random
 from log_config import configure_logger
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-
-
-class UserState:
-    """
-    Состояние пользователя внутри сценария.
-    Атрибуты класса: име текущего сценария, имя текуего шага,
-    контекст (словарь с ответами пользователя).
-    Атрибуты класса принимают новые значения при каждом продвижении пользователя по сценарию.
-    """
-
-    def __init__(self, scenario_name, step_name, context=None):
-        self.scenario_name = scenario_name
-        self.step_name = step_name
-        self.context = context or {}
 
 
 class Bot:
@@ -47,7 +38,6 @@ class Bot:
         self.vk = vk_api.VkApi(token=token)
         self.long_poller = VkBotLongPoll(self.vk, self.group_id)
         self.api = self.vk.get_api()
-        self.user_states = dict()  # user_id -> UserState
 
     def run(self):
         """
@@ -61,25 +51,26 @@ class Bot:
             except Exception:
                 self.bot_logger.exception('При обработке события возникла ошибка.')
 
+    @db_session
     def on_event(self, event):
         """
         Обработка событий из личных сообщений с сообществом.
 
         Если пришедшее сообщение содержится в интентах, вызывается соответствующий сценарий.
-        Если при этом пользователь, от которого пришло сообщение, уже содержится в self.user_states,
-        пользователь удаляется из словаря.
+        Если при этом пользователь, от которого пришло сообщение, уже содержится в таблице UserState,
+        пользователь удаляется из таблицы.
         Если сообщение не соответствует ни одному из интентов, пользователю отправляется случайное
         сообщение из ответов по умолчанию.
 
-        Старт сценария  - вызов метода self.start_scenario()
+        Старт сценария  - вызов метода start_scenario()
 
-        Если сообщение пользователя не содержится в интентах, но сам пользователь есть в словаре self.user_states,
-        вызывается метод продолженя сценария - self.continue_scenario()
+        Если сообщение пользователя не содержится в интентах, но сам пользователь есть в таблице UserState,
+        вызывается метод продолженя сценария - continue_scenario()
 
         Оба основных метода (старта и продолжения сценария) возвращают строку - ответ пользователю.
         Подробнее о формировании ответа см. функции обработчиков в handlers.py и второстепенных методах класса Bot.
 
-        Получив сформированный ответ пользователя от self.start_scenario(), self.continue_scenario()
+        Получив сформированный ответ пользователя от start_scenario(), continue_scenario()
         или ответов по умолчанию, бот отправляет пользователю ответ.
 
         :param event: VkBotMessageEvent object
@@ -88,20 +79,22 @@ class Bot:
 
         if event.type != VkBotEventType.MESSAGE_NEW:
             self.bot_logger.ERROR(f'Мы пока не умеем обрабатывать события типа {event.type}')
-        user_id = event.object.message['peer_id']
+        user_id = str(event.object.message['peer_id'])
         user_text = event.object.message["text"].lower()
+        state = UserState.get(user_id=user_id)
+
         for intent in settings.INTENTS:
             if user_text == intent['command']:
-                if user_id in self.user_states:
-                    self.user_states.pop(user_id)
+                if state is not None:
+                    state.delete()
                 if intent['answer']:
                     response = intent['answer']
                 else:
                     response = self.start_scenario(user_id=user_id, scenario=intent['scenario'])
                 break
         else:
-            if user_id in self.user_states:
-                response = self.continue_scenario(user_id=user_id, user_text=user_text)
+            if state is not None:
+                response = self.continue_scenario(state=state, user_text=user_text)
             else:
                 response = random.choice(settings.DEFAULT_ANSWERS)
 
@@ -109,13 +102,13 @@ class Bot:
         self.bot_logger.info(f'RESPONSE : {response}')
         self.api.messages.send(message=response,
                                random_id=random.randint(0, 2 ** 10000),
-                               peer_id=user_id)
+                               peer_id=int(user_id))
 
     def start_scenario(self, user_id, scenario):
         """
         Метод старта сценария.
 
-        В словарь пользователей добавляется текущий пользователь, ему присваивается имя сценария,
+        В таблицу UserState добавляется текущий пользователь, ему присваивается имя сценария,
         имя текущего шага (берётся из самого сценария).
         Возвращает значение поля text из текущего сценария на текущем шаге.
 
@@ -125,11 +118,14 @@ class Bot:
         """
 
         step = settings.SCENARIOS[scenario]['first_step']
-        self.user_states[user_id] = UserState(scenario_name=scenario, step_name=step)
+        user = self.vk.method('users.get', {'user_ids': user_id, 'fields': 'photo_max'})
+        user_fullname = f'{user[0]["first_name"]} {user[0]["last_name"]}'
+        UserState(user_id=user_id, name=user_fullname, photo=user[0]["photo_max"],
+                  scenario_name=scenario, step_name=step, context={})
         result = settings.SCENARIOS[scenario]['steps'][step]['text']
         return result
 
-    def continue_scenario(self, user_id, user_text):
+    def continue_scenario(self, state, user_text):
         """
         Метод продолжения сценария.
 
@@ -137,18 +133,17 @@ class Bot:
         Если имя обработчика city_handler - к аргументам для вызова обработчика добавляется дополнительный
         аргумент step - подробнее об этом см. docstrings обработчика.
         Если обработчик возвращает False, возвращается текст из поля failure_text из текущего шага.
-        Если обработчик возвращает True, вызывается метод self.extra_print()
+        Если обработчик возвращает True, вызывается метод extra_print()
         (подробнее про метод см. docstrings метода),
         к возвращённому методом значению конкатенирует значение из поля text следующего шага сценария.
-        Если следующий шаг is None, пользователь удалется из словаря пользователей,
+        Если следующий шаг is None, билет добавляется в таблицу Registration,
+        пользователь удалется из таблицы UserState,
         возвращается значение поля text текущего шага сценария. Таким образом, сценарий завершается.
 
-        :param user_id: int, id пользователя
+        :param state: UserState, запись пользователя ил таблицы UserState
         :param user_text: str, сообщение пользователя
         :return: str, ответ пользователю из текста сценария после обработки хендлером
         """
-
-        state = self.user_states[user_id]
 
         steps = settings.SCENARIOS[state.scenario_name]['steps']
         curr_step = steps[state.step_name]
@@ -164,16 +159,18 @@ class Bot:
                 state.step_name = curr_step['next_step']
             else:
                 result = next_step['text']
-                self.user_states.pop(user_id)
+                self._create_and_send_ticket(state=state)
+                Registration(name=state.name, context=state.context)
+                state.delete()
                 return result
             result = self.extra_print(state=state, step=state.step_name)
             if 'Подход' in result:
-                self.user_states.pop(user_id)
+                state.delete()
             else:
                 result += steps[state.step_name]['text']
         else:
             if handler.__name__ == 'data_checking_handler':
-                self.user_states.pop(user_id)
+                state.delete()
             result = curr_step['failure_text']
         return result
 
@@ -185,16 +182,16 @@ class Bot:
         4_flight_selection (выбор из списка рейсов),
         7_data_checking (подтверждение введённой информации).
 
-        Если текущий шаг == выбор из списка рейсов, вызывается метод self.dispatcher()
+        Если текущий шаг == выбор из списка рейсов, вызывается метод dispatcher()
         (подробнее о работе метода см. docstrings метода).
         Возвращает строку - либо сообщение о том, что подходящих рейсов нет
-        (сценарий полностью завершается в методе self.continue_scenario() выше по стеку),
+        (сценарий полностью завершается в методе continue_scenario() выше по стеку),
         либо список подходящих рейсов.
 
         Если текущий шаг == подтверждение введённой информации, возвращает строку - конкатенацию всех данных
-        пользователя (его выборов из предыдущих шагов сценария) из конекста пользователя.
+        пользователя (его выборов из предыдущих шагов сценария) из контекста пользователя.
 
-        :param state: UserState, объект пользователя из словаря пользователей
+        :param state: UserState, запись пользователя ил таблицы UserState
         :param step: str, имя текущего шага
         :return: str, дополнительный текст для ответ пользователю
         """
@@ -202,7 +199,6 @@ class Bot:
         context = state.context
         res = ''
         if '4' in step:
-            context['tickets'] = []
             from_city = context['departure_city']
             to_city = context['arrival_city']
             disp_res = self.dispatcher(context=context, from_city=from_city, to_city=to_city)
@@ -211,10 +207,12 @@ class Bot:
             else:
                 res += disp_res
         elif '7' in step:
+            context["departure_city"] = context["departure_city"].capitalize()
+            context["arrival_city"] = context["arrival_city"].capitalize()
             res += f'Проверка данных авиабилета (стоимость билета указана за одно место):\n....' \
-                   f'Откуда: {context["departure_city"].capitalize()}\n....' \
-                   f'Куда: {context["arrival_city"].capitalize()}\n....' \
-                   f'Дата: {context["flight"]["date"]}\n....Стоимость билета: {context["flight"]["price"]}\n....' \
+                   f'Откуда: {context["departure_city"]}\n....' \
+                   f'Куда: {context["arrival_city"]}\n....' \
+                   f'Дата: {context["date"]}\n....Стоимость билета: {context["price"]}\n....' \
                    f'Количество мест: {context["number_of_seats"]}\n....' \
                    f'Комментарий: {context["comment"].capitalize()}\n\n'
         return res
@@ -248,6 +246,24 @@ class Bot:
                     temp_ticket = f'Дата: {flight[0]}\n....Стоимость билета: {flight[1]}'
                     res += f'{i + 1})\n....{temp_ticket}\n\n'
         return res
+
+    def _create_and_send_ticket(self, state):
+        """
+        Создание и отправка билета.
+
+        :param state: UserState, запись пользователя ил таблицы UserState
+        :return: None
+        """
+
+        ticket_maker = TicketMaker(data=state)
+        ticket_bytes = ticket_maker.draw()
+        upload_url = self.api.photos.getMessagesUploadServer()['upload_url']
+        upload_data = requests.post(url=upload_url, files={'photo': ('image.png', ticket_bytes, 'image/png')}).json()
+        ticket_data = self.api.photos.saveMessagesPhoto(**upload_data)
+        attachment = f'photo{ticket_data[0]["owner_id"]}_{ticket_data[0]["id"]}'
+        self.api.messages.send(attachment=attachment,
+                               random_id=random.randint(0, 2 ** 10000),
+                               peer_id=int(state.user_id))
 
 
 if __name__ == '__main__':
